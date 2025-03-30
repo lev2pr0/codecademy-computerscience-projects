1254bf918097fececd5e5c8870541ebb1ec011cc
Levar Norwood <46905650+lev2pr0@users.noreply.github.com>
2025-03-29 20:47:08 -0400

    Create blockletter.py

---
+++ blockletter.py
@@ -0,0 +1,30 @@
+#Levar Norwood
+#I pushed off coding for 6-8 years until I received ADHD medication and now addicted to learning it :)
+
+
+#Initials in ASCII Art
+#First Initial 'L'
+L_rows = [
+ "L          ",
+ "L          ",
+ "L          ",
+ "L          ",
+ "L          ", 
+ "L          ",
+ "L L L L L L"
+]
+
+#Last Initial 'N'
+N_rows =[
+ "  N       N",
+ "  N N     N",
+ "  N   N   N",
+ "  N     N N",
+ "  N       N",
+ "  N       N",
+ "  N       N"
+]
+
+#Print the initials
+for i in range(7):
+    print(L_rows[i] + N_rows[i])
