3c7020b47675b4a364dcdb03d949ccd8643adc74
Levar Norwood <46905650+lev2pr0@users.noreply.github.com>
2025-04-01 07:29:52 -0400

    spell checking

--- LovelyseatsReceipts.py
+++ LovelyseatsReceipts.py
@@ -1,4 +1,4 @@
-#Lovely LoveSeat Reciept System
+#Lovely LoveSeat Receipt System
 
 #Catalog Description
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
