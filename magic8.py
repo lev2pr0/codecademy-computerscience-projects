b0963406f4549b505ea167a4a89335a4b6c8e910
Levar Norwood <46905650+lev2pr0@users.noreply.github.com>
2025-04-02 20:40:37 -0400

    completed project

--- magic8.py
+++ magic8.py
@@ -1,6 +1,6 @@
 import random
 
-# List of magic 8 ball answers
+# List of Magic 8 ball's answers
 magic8_response = ["Yes - definitely",
             "It is decidedly so",
             "Without a doubt",
@@ -11,10 +11,20 @@ magic8_response = ["Yes - definitely",
             "Outlook not so good",
             "Very doubtful"]
 
-print("What is your name?")
-#need to fill with input variable
-print("Present your question:")
-#need to fill with input variable
+# Magic 8 ball Program Welcome
+print("Welcome to the Magic 8-Ball!" + "\n")
+
+# Ask for user's name and question
+print("What is your name?" + "\n")
+name = input("What is your name? ")
+print("Hello " + name + "!" + "\n")
+print("Ask me a yes or no question, and I will give you an answer." + "\n")
+question = input("What is your question? ")
+
+# Print the user's question
+print(f"{name}, asks: {question}\n")
+
+# Generate a random answer from the Magic 8 ball
 magic8_answer = random.choice(magic8_response)
 print(f"Magic 8-Ball’s answer: {magic8_answer}")
 
